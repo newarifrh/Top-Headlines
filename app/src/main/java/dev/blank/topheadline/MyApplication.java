5e96f3d02cd8431c959e290acccf5e5df1b09628
Arif Rahman Hakim <16.9025@stis.ac.id>
2020-03-06 17:40:53 +0700

    disable multidex

--- app/src/main/java/dev/blank/topheadline/MyApplication.java
+++ app/src/main/java/dev/blank/topheadline/MyApplication.java
@@ -2,9 +2,12 @@ package dev.blank.topheadline;
 
 import android.app.Application;
 
+import androidx.multidex.MultiDex;
+
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(this);
     }
 }
